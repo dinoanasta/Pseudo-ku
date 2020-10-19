d845bd45f156b6b0d0b339bbc798dcd546f2a408
Reece <61417493+ReeceJamesPeters@users.noreply.github.com>
2020-10-19 15:57:49 +0200

    Update Program.java

--- Program/Program.java
+++ Program/Program.java
@@ -10,7 +10,7 @@ public class Program {
 		// runForSubmission();
 
 		//Run for testing purposes
-		runForTesting();
+		runForTestingNew();
 	}
 
 	public static boolean isSafe(int[][] board, int row, int col, int num) {
@@ -116,6 +116,38 @@ public class Program {
 		}
 	}
 
+	public static void removeCluesOrdered(int[][] board, int numClues) {
+			for (int row = 0; row < 9; row++) {
+				for (int column = 0; column < 9; column++) {
+					if(board[row][column]!=0){
+						board[row][column]=0;
+						numClues--;
+					}
+					if(numClues<=0){
+						break;
+					}
+				}
+				if(numClues<=0){
+						break;
+					}
+			}
+	}
+	
+
+	public static void removeCluesRandom(int[][] board, int numClues) {
+		while(numClues>0){
+			Random r = new Random();
+			int row = r.nextInt(9);
+			int column = r.nextInt(9);
+			if(board[row][column]!=0){
+				board[row][column]=0;
+				numClues--;
+			}
+		}
+	}
+
+
+
 	public static void runForSubmission()  throws IOException {
 		Scanner in = new Scanner(System.in);
 
@@ -213,6 +245,111 @@ public class Program {
 
 				changesOutput.append(difc + "," + changes);
 				changesOutput.append("\n");
+
+
+			}
+			timeOutput.flush();
+			timeOutput.close();
+
+			comparisonsOutput.flush();
+			comparisonsOutput.close();
+
+			changesOutput.flush();
+			changesOutput.close();
+
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+	}
+
+	public static void runForTestingNew()  throws IOException {
+		try {
+			//Create new .csv file
+			FileWriter timeOutput = new FileWriter("bull1.csv");
+			timeOutput.append("EmptyCells");
+			timeOutput.append(",");
+			timeOutput.append("Time");
+			timeOutput.append("\n");
+
+			FileWriter comparisonsOutput = new FileWriter("bull2.csv");
+			comparisonsOutput.append("EmptyCells");
+			comparisonsOutput.append(",");
+			comparisonsOutput.append("Comparisons");
+			comparisonsOutput.append("\n");
+
+			FileWriter changesOutput = new FileWriter("bull3.csv");
+			changesOutput.append("EmptyCells");
+			changesOutput.append(",");
+			changesOutput.append("Changes");
+			changesOutput.append("\n");
+	
+			//Open file
+			File myObj = new File("COMPLETED_PUZZLES.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				// Take in puzzles
+				int difc = Integer.parseInt(myReader.nextLine());
+				int[][] board = new int[9][9];
+
+				for (int i = 0; i < 9; ++i) {
+					String[] line = myReader.nextLine().split(" ");
+					for (int j = 0; j < 9; j++) {
+						board[i][j] = Integer.parseInt(line[j]);
+					}
+				}
+				
+				for(int g=0; g<=81; g++){
+
+					//reset counter
+					counter[0]=0;
+					counter[1]=0;
+
+					//remove clue
+					removeCluesRandom(board,g);
+
+					//Print out original puzzle
+					System.out.println("----- PUZZLE: -----");
+					printForTesting(board);
+
+					//Start timer
+					long startTime = System.nanoTime();
+
+					//Solve and print solution, or print "No solution"
+					if (solveSudoku(board)) {
+						System.out.println("----- SOLUTION: -----");
+						printForTesting(board);
+					} else {
+						System.out.println("No solution");
+					}
+
+					//End timer
+					long endTime = System.nanoTime();
+					long duration = (endTime - startTime) / 1000;
+
+					int comparisons = counter[0];
+					int changes = counter[1];
+
+					System.out.println("Board Number: " + difc);
+					System.out.println(Long.toString(duration) + " microseconds");
+					System.out.println(Integer.toString(comparisons) + " comparisons");
+					System.out.println(Integer.toString(changes) + " changes");
+					System.out.println("\n");
+					System.out.println("=======================================================");
+					System.out.println("\n");
+
+					timeOutput.append(g + "," + duration);
+					timeOutput.append("\n");
+
+
+					comparisonsOutput.append(g + "," + comparisons);
+					comparisonsOutput.append("\n");
+
+
+					changesOutput.append(g + "," + changes);
+					changesOutput.append("\n");
+				}
 
 
 			}
